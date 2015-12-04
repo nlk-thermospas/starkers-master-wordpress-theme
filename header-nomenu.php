605c4e9e73f4e0f204c2805bc316bc93238f7321
AmitDeo <magpie.arise@gmail.com>
2015-12-04 09:36:27 +0545

    Removed menu and search bar from Get pricing

---
+++ header-nomenu.php
@@ -0,0 +1,24 @@
+<div class="container">
+	<header class="header">
+		<div class="utility">
+			<ul>
+				<li class="call"><a href="tel:1-800-876-0158">Call Us <strong>1-800-876-0158</strong></a></li>
+				<li class="pricing"><a href="/get-pricing">Get Pricing</a></li>
+			</ul>
+		</div>
+	</header>
+</div>
+<div class="stripe-container">
+	<div class="stripe">
+		<div class="logo">
+			<a href="/"><img src="/dress/global/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
+		</div>
+
+		<?php $uri = $_SERVER['REQUEST_URI'];
+			$uriArr = explode('/', $uri);
+		?>
+
+	</div>
+</div>
+
+<div class="container">
