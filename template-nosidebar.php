6d8a37bccbfeb4740244b1f3843b50ade2436ab3
AmitDeo <magpie.arise@gmail.com>
2016-01-19 22:04:07 +0545

    Fullwidth No Sidebar template

--- template-nosidebar.php
+++ template-nosidebar.php
@@ -41,23 +41,7 @@
 
 		<div class="primary">
 			<article>
-				<div class="grid getpricingnew">
-				  <div class="grid__col grid__col--6-of-12">
-				    <?php the_content(); ?>
-				  </div>
-				  <div class="grid__col grid__col--6-of-12">
-				    <div class="hero-box">
-						<div class="grid">
-							<div class="grid__col grid__col--7-of-12 hero-box-text">
-								<?php the_field('call-outs'); ?>
-							</div>
-							<div class="grid__col grid__col--5-of-12">
-								<div class="hero-box-image"></div>
-							</div>
-						</div>
-					</div>
-				  </div>
-				</div>
+				<?php the_content(); ?>
 			</article>
 		</div>
 
